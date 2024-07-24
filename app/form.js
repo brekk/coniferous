fd27b497d3b870bf6ed6d0492a3a6cf5c8603429
brekk <brekk@brekkbockrath.com>
2024-07-23 17:40:08 -1000

    add review-and-pay sub-div

--- app/form.js
+++ app/form.js
@@ -5,34 +5,44 @@ import Button from "./button";
 
 export const Form = ({ checkValid, formDataProps, goForward }) => {
   const isValid = checkValid();
-  console.log("IS VALID?!", isValid);
   const bem = blem("Form");
   return (
-    <div className={bem("form-wrapper")}>
-      <h2 className={bem("form-heading")}>
-        <span className={bem("form-step", "1")}>1</span>
-        Payment information
-      </h2>
-      <div className={bem("form")}>
-        <FormField label="Card number" {...formDataProps("creditCard")} />
-        <div className={bem("across", ["two"])}>
-          <FormField label="Expires (MM/YY)" {...formDataProps("expiration")} />
-          <FormField label="Security Code (CVV)" {...formDataProps("cvv")} />
+    <>
+      <div className={bem("form-wrapper")}>
+        <h2 className={bem("form-heading", "active")}>
+          <span className={bem("form-step", "active")}>1</span>
+          Payment information
+        </h2>
+        <div className={bem("form")}>
+          <FormField label="Card number" {...formDataProps("creditCard")} />
+          <div className={bem("across", ["two"])}>
+            <FormField
+              label="Expires (MM/YY)"
+              {...formDataProps("expiration")}
+            />
+            <FormField label="Security Code (CVV)" {...formDataProps("cvv")} />
+          </div>
+          <FormField label="Name on card" {...formDataProps("name")} />
+          <FormField label="ZIP code" {...formDataProps("zip")} />
+          <Button
+            onClick={() => {
+              if (isValid) {
+                goForward();
+              }
+            }}
+            disabled={!isValid}
+          >
+            Continue
+          </Button>
         </div>
-        <FormField label="Name on card" {...formDataProps("name")} />
-        <FormField label="ZIP code" {...formDataProps("zip")} />
-        <Button
-          onClick={() => {
-            if (isValid) {
-              goForward();
-            }
-          }}
-          disabled={!isValid}
-        >
-          Continue
-        </Button>
       </div>
-    </div>
+      <div className={bem("review-and-pay")}>
+        <h2 className={bem("form-heading", "inactive")}>
+          <span className={bem("form-step", "inactive")}>2</span>
+          Review and pay
+        </h2>
+      </div>
+    </>
   );
 };
 
